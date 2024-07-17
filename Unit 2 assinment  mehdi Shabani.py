53b73081c094fa610cd0c65e6518bb41d859b74b
shabanimehdi84 <shabanimehdi84@yahoo.com>
2024-07-16 21:34:12 -0500

    Add files via upload

---
+++ Unit 2 assinment  mehdi Shabani.py
@@ -0,0 +1,11 @@
+def Add(x, y):
+    return x + y
+def    runtest() :
+ assert  Add(1, 2) == 3
+ assert  Add(-1, 2) == 1
+ assert  Add(1, -2) == -1
+ assert  Add(-1, -2) == -3
+Result = Add(1, 2); assert Result == 3
+print("All tests passed .")
+
+
